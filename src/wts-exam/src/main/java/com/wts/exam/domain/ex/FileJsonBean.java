3b1db53843a8c7a562832eb8dc139f842e80fc82
macplus <macpaohui@126.com>
2020-04-10 09:20:47 +0800

    no commit message

---
+++ src/wts-exam/src/main/java/com/wts/exam/domain/ex/FileJsonBean.java
@@ -0,0 +1,24 @@
+package com.wts.exam.domain.ex;
+
+import com.farm.doc.domain.FarmDocfile;
+
+public class FileJsonBean {
+	private FarmDocfile info;
+	private String base64;
+
+	public FarmDocfile getInfo() {
+		return info;
+	}
+
+	public void setInfo(FarmDocfile info) {
+		this.info = info;
+	}
+
+	public String getBase64() {
+		return base64;
+	}
+
+	public void setBase64(String base64) {
+		this.base64 = base64;
+	}
+}
